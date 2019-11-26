c2081e1a868c42f26db09f2cdfe216d50fd843a1
Hur <hur007@yandex.ru>
2019-11-26 03:51:12 -0500

    wrapped

--- app/src/main/java/com/example/nrip/td_ml_project/BudgetTestActivity.java
+++ app/src/main/java/com/example/nrip/td_ml_project/BudgetTestActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.example.nrip.td_ml_project.models.BudgetCalculator;
 import com.example.nrip.td_ml_project.models.BudgetEntry;
 import com.example.nrip.td_ml_project.models.BudgetPeriod;
 import com.opencsv.CSVReader;
@@ -37,37 +38,28 @@ public class BudgetTestActivity extends AppCompatActivity {
     String TAG ="LogCatDemo";
 
     ArrayList<BudgetEntry> budgetEntries;
+    BudgetCalculator bc;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_budget_calc_test);
-
-
-
+        bc = new BudgetCalculator();
     }
 
     public void onLoadData(View view) {
-        Log.d(TAG, "hi");
         try {
-
             InputStream ins = getResources().openRawResource(
                     getResources().getIdentifier("csv35639",
                             "raw", getPackageName()));
 
-
             CSVReader reader = new CSVReader(new InputStreamReader(ins));
-
             budgetEntries = new ArrayList<>();
             String[] nextLine;
-            Log.d(TAG, "hi");
             int i = 0;
             while ((nextLine = reader.readNext()) != null) {
                 if (i > 0) {
-//                    Log.d(TAG, nextLine[0]);
-//                    Log.d(TAG, String.valueOf(new BigDecimal(nextLine[6]).doubleValue()));
-//                    Log.d(TAG, "=========================");
                     if (new BigDecimal(nextLine[6]).doubleValue() > 0 && nextLine[0].equals("Chequing")){
                         budgetEntries.add((new BudgetEntry(nextLine[0], new SimpleDateFormat("MM/dd/yyyy").parse(nextLine[2]), new BigDecimal(nextLine[6]))));
                     }
@@ -76,82 +68,13 @@ public class BudgetTestActivity extends AppCompatActivity {
                     }
                 }
                 i++;
-                // nextLine[] is an array of values from the line
-                //Log.d(TAG, nextLine[6]);
-                // System.out.println(nextLine[0] + nextLine[1] + "etc...");
-
             }
-            calcBudget();
+            bc.calcBudget(budgetEntries);
         } catch (IOException e) {
             Log.d(TAG, e.toString());
         } catch (ParseException e) {
             Log.d(TAG, e.toString());
             e.printStackTrace();
-        }
-
-
-
-    }
-
-    public void calcBudget() {
-
-        Calendar calendar = new GregorianCalendar();
-        Map<String, BudgetPeriod> budgetMap = new HashMap<>();
-
-
-        for (int i = 0; i < budgetEntries.size(); i++) {
-            //Date trialTime = new Date();
-            calendar.setTime(budgetEntries.get(i).Date);
-//            Log.d(TAG, "Week number:" +
-//                    calendar.get(Calendar.WEEK_OF_YEAR));
-            calendar.get(Calendar.YEAR);
-
-            String key = String.format("%1$s-%2$s",calendar.get(Calendar.YEAR), calendar.get(Calendar.WEEK_OF_YEAR));
-            BudgetPeriod bp;// = budgetMap.get(key);
-
-            if (budgetMap.containsKey(key)){
-                bp = budgetMap.get(key);
-                //Log.d(TAG, key + "exists");
-            }
-            else {
-                bp = new BudgetPeriod(calendar.get(Calendar.WEEK_OF_YEAR), calendar.get(Calendar.YEAR));
-                //Log.d(TAG, key + "is new");
-            }
-
-            bp.Entries.add(budgetEntries.get(i));
-            budgetMap.put(key, bp);
-        }
-
-        Log.d(TAG, "map size:" +
-                budgetMap.size());
-
-        BigDecimal totalDeposit = BigDecimal.ZERO;
-        BigDecimal totalWithdrawal = BigDecimal.ZERO;
-
-        for(String key : budgetMap.keySet()){
-            totalDeposit = totalDeposit.add(budgetMap.get(key).CalcDeposits());
-            totalWithdrawal = totalWithdrawal.add(budgetMap.get(key).CalcWithdrawals());
-        }
-
-        BigDecimal avgWeeklyDeposit = totalDeposit.divide(new BigDecimal(budgetMap.size()),2, RoundingMode.HALF_EVEN);
-        BigDecimal avgWeeklyWithdrawal = totalWithdrawal.divide(new BigDecimal(budgetMap.size()),2, RoundingMode.HALF_EVEN);
-
-        Log.d(TAG, "totalDeposit: $" + totalDeposit);
-        Log.d(TAG, "totalWithdrawal: $" + totalWithdrawal);
-        Log.d(TAG, "avgWeeklyDeposit: $" + avgWeeklyDeposit);
-        Log.d(TAG, "avgWeeklyWithdrawal: $" + avgWeeklyWithdrawal);
-        Log.d(TAG, "avgWeeklyBudget: $" + avgWeeklyDeposit.subtract(avgWeeklyWithdrawal));
-    }
-
-    public static BigDecimal round(BigDecimal value, BigDecimal increment,
-                                   RoundingMode roundingMode) {
-        if (increment.signum() == 0) {
-            // 0 increment does not make much sense, but prevent division by 0
-            return value;
-        } else {
-            BigDecimal divided = value.divide(increment, 0, roundingMode);
-            BigDecimal result = divided.multiply(increment);
-            return result;
         }
     }
 }
---
+++ app/src/main/java/com/example/nrip/td_ml_project/models/BudgetCalculator.java
@@ -0,0 +1,69 @@
+package com.example.nrip.td_ml_project.models;
+
+import android.util.Log;
+import android.view.View;
+
+import com.opencsv.CSVReader;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BudgetCalculator {
+
+    String TAG ="LogCatDemo";
+
+
+
+
+    public BigDecimal calcBudget(ArrayList<BudgetEntry> budgetEntries) {
+
+        Calendar calendar = new GregorianCalendar();
+        Map<String, BudgetPeriod> budgetMap = new HashMap<>();
+
+        for (int i = 0; i < budgetEntries.size(); i++) {
+            calendar.setTime(budgetEntries.get(i).Date);
+            calendar.get(Calendar.YEAR);
+
+            String key = String.format("%1$s-%2$s",calendar.get(Calendar.YEAR), calendar.get(Calendar.WEEK_OF_YEAR));
+            BudgetPeriod bp;
+
+            if (budgetMap.containsKey(key)){
+                bp = budgetMap.get(key);
+            }
+            else {
+                bp = new BudgetPeriod(calendar.get(Calendar.WEEK_OF_YEAR), calendar.get(Calendar.YEAR));
+            }
+
+            bp.Entries.add(budgetEntries.get(i));
+            budgetMap.put(key, bp);
+        }
+
+        BigDecimal totalDeposit = BigDecimal.ZERO;
+        BigDecimal totalWithdrawal = BigDecimal.ZERO;
+
+        for(String key : budgetMap.keySet()){
+            totalDeposit = totalDeposit.add(budgetMap.get(key).CalcDeposits());
+            totalWithdrawal = totalWithdrawal.add(budgetMap.get(key).CalcWithdrawals());
+        }
+
+        BigDecimal avgWeeklyDeposit = totalDeposit.divide(new BigDecimal(budgetMap.size()),2, RoundingMode.HALF_EVEN);
+        BigDecimal avgWeeklyWithdrawal = totalWithdrawal.divide(new BigDecimal(budgetMap.size()),2, RoundingMode.HALF_EVEN);
+
+        Log.d(TAG, "totalDeposit: $" + totalDeposit);
+        Log.d(TAG, "totalWithdrawal: $" + totalWithdrawal);
+        Log.d(TAG, "avgWeeklyDeposit: $" + avgWeeklyDeposit);
+        Log.d(TAG, "avgWeeklyWithdrawal: $" + avgWeeklyWithdrawal);
+        Log.d(TAG, "avgWeeklyBudget: $" + avgWeeklyDeposit.subtract(avgWeeklyWithdrawal));
+        return avgWeeklyDeposit.subtract(avgWeeklyWithdrawal);
+    }
+}
